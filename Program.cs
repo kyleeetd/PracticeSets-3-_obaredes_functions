eef8453386e41f3efb798ed855826a68fa6d7ebb
kyleeetd <159506702+kyleeetd@users.noreply.github.com>
2024-09-21 11:32:40 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double a = 5;
+        double b = 7;
+        double c = 4;
+
+        double area = TriangleArea(a, b, c);
+
+        Console.WriteLine($"Area of the triangle: {area}");
+    }
+    static double TriangleArea(double a, double b, double c)
+    {
+
+        double s = (a + b + c) / 2;
+
+        double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+
+        return area;
+    }
+}
